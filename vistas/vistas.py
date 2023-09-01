9171156b09aff738777ef5911d645e5525131de8
andsoler0309 <andsoler0309@users.noreply.github.com>
2023-09-01 19:54:52 +0000

    :art: Format Python code with psf/black push

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -21,7 +21,7 @@ from modelos import (
     Rol,
     MenuSemana,
     MenuSemanaSchema,
-    MenuReceta
+    MenuReceta,
 )
 
 ingrediente_schema = IngredienteSchema()
@@ -30,6 +30,7 @@ receta_schema = RecetaSchema()
 usuario_schema = UsuarioSchema()
 restaurante_schema = RestauranteSchema()
 menu_semana_schema = MenuSemanaSchema()
+
 
 class VistaSignIn(Resource):
     def post(self):
@@ -267,6 +268,7 @@ class VistaReceta(Resource):
 
         return receta_ingrediente_retornar
 
+
 class VistaRestaurantes(Resource):
     @jwt_required()
     def post(self, id_usuario):
@@ -330,6 +332,7 @@ class VistaRestaurantes(Resource):
             error_message = "Error while querying the database"
             return {"error": error_message}, 500
 
+
 class VistaMenuSemana(Resource):
     @jwt_required()
     def get(self):
@@ -338,32 +341,39 @@ class VistaMenuSemana(Resource):
 
     @jwt_required()
     def post(self):
-        nombre_menu_repetido = MenuSemana.query.filter_by(nombre=request.json["nombre"]).first()
+        nombre_menu_repetido = MenuSemana.query.filter_by(
+            nombre=request.json["nombre"]
+        ).first()
         if nombre_menu_repetido is not None:
             return "El nombre del menu ya existe", 400
         try:
-            fecha_inicial = datetime.strptime(request.json["fecha_inicial"],'%Y-%m-%d').date()
-            fecha_final = datetime.strptime(request.json["fecha_final"],'%Y-%m-%d').date()
+            fecha_inicial = datetime.strptime(
+                request.json["fecha_inicial"], "%Y-%m-%d"
+            ).date()
+            fecha_final = datetime.strptime(
+                request.json["fecha_final"], "%Y-%m-%d"
+            ).date()
         except Exception as e:
             return str(e), 400
         diff_fecha = fecha_final - fecha_inicial
-        if diff_fecha.days !=6:
+        if diff_fecha.days != 6:
             return "Las fechas no tienen la diferencia correcta", 400
 
         todos_menus = MenuSemana.query.all()
         for menu in todos_menus:
-            if (fecha_final >= menu.fecha_final >= fecha_inicial) or \
-                    (fecha_final >= menu.fecha_inicial >= fecha_inicial):
+            if (fecha_final >= menu.fecha_final >= fecha_inicial) or (
+                fecha_final >= menu.fecha_inicial >= fecha_inicial
+            ):
                 return "Las fechas tienen conflicto con las de otro menu", 400
 
-        nuevo_menu_semana = MenuSemana( \
-            nombre=request.json["nombre"], \
-            fecha_inicial=fecha_inicial, \
+        nuevo_menu_semana = MenuSemana(
+            nombre=request.json["nombre"],
+            fecha_inicial=fecha_inicial,
             fecha_final=fecha_final,
-            )
+        )
         for receta_id in request.json["recetas"]:
-            receta_menu = MenuReceta(menu=nuevo_menu_semana.id,receta=receta_id)
+            receta_menu = MenuReceta(menu=nuevo_menu_semana.id, receta=receta_id)
             nuevo_menu_semana.recetas.append(receta_menu)
         db.session.add(nuevo_menu_semana)
         db.session.commit()
-        return menu_semana_schema.dump(nuevo_menu_semana),200
+        return menu_semana_schema.dump(nuevo_menu_semana), 200
